15a8c88000617c728959a71c3e56ad6f1de7600a
Ferdinand05 <fkoryanto@gmail.com>
2026-02-15 13:12:58 +0700

    feat(service) : add service folder for ticket

--- app/Filament/Resources/Tickets/Pages/ListTickets.php
+++ app/Filament/Resources/Tickets/Pages/ListTickets.php
@@ -5,20 +5,24 @@ namespace App\Filament\Resources\Tickets\Pages;
 use App\Filament\Pages\ScanTicket;
 use App\Filament\Resources\Tickets\TicketResource;
 use App\Models\Distribution;
-use App\Models\Household;
-use App\Models\Ticket;
 use Filament\Actions\Action;
 use Filament\Forms\Components\Select;
-use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Icons\Heroicon;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use TicketService;
 
 class ListTickets extends ListRecords
 {
     protected static string $resource = TicketResource::class;
+    protected $ticketService;
+
+    public function __construct(TicketService $ticketService)
+    {
+
+        $this->ticketService = $ticketService;
+    }
+
+
 
     protected function getHeaderActions(): array
     {
@@ -47,63 +51,9 @@ class ListTickets extends ListRecords
                         ->required()
                 ])
                 ->action(function (array $data) {
-                    $distribution = Distribution::find($data["distribution_id"]);
 
-                    // Generate tiket untuk distribusi ini
-                    $households = Household::whereDoesntHave('tickets', function ($query) use ($distribution) {
-                        $query->where('distribution_id', $distribution->id);
-                    })->get();
-
-                    $total = $households->count();
-
-                    if ($total === 0) {
-                        Notification::make()
-                            ->warning()
-                            ->title('Tidak ada KK yang perlu digenerate')
-                            ->body("Semua KK sudah memiliki tiket untuk {$distribution->name}")
-                            ->send();
-                        return;
-                    }
-
-                    $generated = 0;
-
-                    // generate ticket
-                    foreach ($households->chunk(30) as $chunk) {
-                        foreach ($chunk as $household) {
-                            $token = (string) Str::uuid();
-
-                            // generate qr dengan format svg
-                            $svg = QrCode::format('svg')
-                                ->size(300)
-                                ->margin(1)
-                                ->color(0, 0, 0)
-                                ->backgroundColor(255, 255, 255)
-                                ->generate($token);
-
-                            // path file dan save ke storage
-                            $filename = 'qrcodes/' . $token . '.svg';
-                            Storage::disk('public')->put($filename, $svg);
-
-                            // create ticket
-                            Ticket::create([
-                                'household_id' => $household->id,
-                                'distribution_id' => $distribution->id,
-                                'token' => $token,
-                                'status' => 'issued',
-                                'qr_code_path' => $filename,
-                                'issued_at' => now(),
-                            ]);
-
-                            $generated++;
-                        }
-                    }
-
-                    Notification::make()
-                        ->success()
-                        ->title(" Berhasil generate {$generated} tiket + QR Code")
-                        ->body("Disimpan di: storage/qrcodes")
-                        ->icon(Heroicon::CheckCircle)
-                        ->send();
+                    // generate mass ticket service
+                    $this->ticketService->generateMassTicket($data);
                 })
                 ->color("success"),
 
